bde5841ca02cb9caa916a98a9123d54264ed43b0
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-12-02 11:16:46 +0530

    Create c_scan.c

---
+++ os lab/c_scan.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n-1; i++)
+    {
+       
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        if(min_idx!=i)
+            swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number tracks containing data\n");
+    scanf("%d",&n);
+    int D[n+3];
+    printf("Enter the tracks containing data\n");
+    for(int i = 0;i<n;i++)
+    {
+        scanf("%d",&D[i]);
+    }
+    int head,r1,r2;
+    printf("Enter the disk actuator arm track\n");
+    scanf("%d",&head);
+    printf("Enter the range of the track\n");
+    scanf("%d%d",&r1,&r2);
+    int seek_time = 0;
+    
+    D[n]=head;
+    D[n+1]=r1;
+    D[n+2]=r2;
+    
+    selectionSort(D,n+3);
+    
+    int idx;
+    for(int i = 0;i<n+3;i++)
+    {
+        if(head == D[i])
+        {
+            idx = i;
+            break;
+        }
+    }
+    
+    int curr = D[idx];
+    for(int i= idx+1;i<n+3;i++)
+    {
+        seek_time+=abs(D[i]-curr);
+        curr=D[i];
+    }
+    
+    for(int i=0;i<idx;i++)
+    {
+        seek_time+=abs(D[i]-curr);
+        curr=D[i];
+    }
+    printf("Seek time is %d\n",seek_time);
+    return 0;
+}
