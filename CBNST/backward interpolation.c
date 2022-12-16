4e8ca62b838f05e185dc53c576a8f3a31757d0f3
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-12-16 20:10:26 +0530

    Create backward interpolation.c

---
+++ CBNST/backward interpolation.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define max 100
+
+int main()
+{
+    float dt[max][max];
+    int n;
+    printf("Enter the number of data points");
+    scanf("%d",&n);
+    
+    printf("enter the data points");
+    for(int i = 0;i<n;i++)
+    {
+        scanf("%f%f",&dt[i][0],&dt[i][1]);
+    }
+   
+   float h = dt[1][0]-dt[0][0];
+   float x;
+   printf("Enter the value of x for which you find y");
+   scanf("%f",&x);
+   float u = (x-dt[0][0])/h;
+   
+   for(int j=2 ;j<=n;j++)
+   {
+       for(int i = n-1;i<j-2;i++)
+       {
+           dt[i][j] = dt[i][j-1]-dt[i-1][j-1];
+       }
+   }
+   
+   float ans = dt[n-1][1];
+   float ch = u;
+   float fact = 1.0;
+   
+   for(int i = 2 ;i<=n;i++)
+   {
+       ans += (ch/fact)*dt[n-1][i];
+       ch *= (u+(float)(i-1));
+       fact *=i;
+   }
+   printf("%f",ans);
+}
